6a51e47f177f6dc42a999d4da43164c6cd5bc656
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-08-26 11:41:43 +0700

    update

--- server/index.js
+++ server/index.js
@@ -6,20 +6,18 @@ const connectdb = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 require("dotenv").config();
 
-connectdb();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-//Run this command app.use() in ALL request GET POST PUT
 //Application-level Middleware
 app.use(cors());
 app.use((req, res, next) => {
   console.log(`New request at: ${Date()}`);
   next("route");
 });
-// app.use(express.json());
-app.use("/api/products", productRoutes); //any request going to routes /api/products will access Router
 
-const PORT = process.env.PORT || 5000;
+//any request going to routes /api/products will access Router
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
- 
+connectdb();
