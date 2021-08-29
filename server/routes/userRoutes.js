2b8c387bf3c73e900d02a870484be0fc51fc7cab
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-08-29 09:28:37 +0700

    Middleware Authorization

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getAllUsers, getUserById } = require("../controller/userController");
+const verify = require("../middleware/authMiddleware");
 
-router.get("/", getAllUsers);
-router.get("/:id", getUserById);
+router.get("/", verify, getAllUsers);
+router.get("/:id", verify, getUserById);
 
 module.exports = router;
