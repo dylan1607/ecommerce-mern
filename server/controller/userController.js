ad22aca069694158c99e718828150a73802b44c7
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-08-29 17:19:25 +0700

    find user by query

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -50,20 +50,47 @@ const updateUser = async (req, res) => {
 //get individual user by user id
 const getUserById = async (req, res) => {
   try {
-    const user = await Product.findById(req.params.id);
+    const user = await User.findById(req.params.id);
     res.json(user);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server Error" });
+    res.status(500).json({ message: "User not exist" });
+  }
+};
+
+//Get user by query
+const getUserByQuery = async (req, res) => {
+  const query = req.query.name;
+  try {
+    const user = await User.find({
+      //Match string in query for case insensitivity. $regex, $options
+      fullname: { $regex: query, $options: "i" },
+    });
+    user.length > 0
+      ? res.status(200).json(user)
+      : res.status(200).json("Not found");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error Server" });
   }
 };
 
 //Delete user by id
-const deleteUser = async (req, res) => {};
+const deleteUser = async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.user.id);
+      res.status(200).json("User has been deleted...");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else res.status(403).json("Your are not allowed");
+};
 
 module.exports = {
   getAllUsers,
   updateUser,
   getUserById,
+  getUserByQuery,
   deleteUser,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllUsers,
-  getUserById,
   updateUser,
+  getUserById,
+  getUserByQuery,
   deleteUser,
 } = require("../controller/userController");
 const verify = require("../middleware/authMiddleware");
@@ -14,8 +15,11 @@ router.get("/", verify, getAllUsers);
 //Update user information by Id
 router.put("/:id", verify, updateUser);
 
-//Find user
-router.get("/find/:id", getUserById);
+//Find user by id
+router.get("/search/:id", getUserById);
+
+//Find user by query
+router.get("/search", getUserByQuery);
 
 //Delete user
 router.delete("/:id", verify, deleteUser);
