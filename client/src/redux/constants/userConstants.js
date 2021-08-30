aae5fa1a3eef3ba12bc8078b2bb75b1c7433650d
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-08-30 10:00:09 +0700

    User Redux

--- client/src/redux/constants/userConstants.js
+++ client/src/redux/constants/userConstants.js
@@ -1,4 +1,7 @@
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
 export const REGISTER = "REGISTER";
-export const LOGIN_FAIL = "LOGIN_FAIL";
+
+export const USER_FAIL = "USER_FAIL";
+export const USER_REQUEST = "USER_REQUEST";
+export const USER_SUCCESS = "USER_SUCCESS";
---
+++ client/src/redux/reducers/userReducer.js
@@ -0,0 +1,23 @@
+import * as actionTypes from "../constants/userConstants";
+
+export const userReducer = (state = { users: [] }, action) => {
+  switch (action.type) {
+    case actionTypes.USER_REQUEST:
+      return {
+        loading: true,
+        users: [],
+      };
+    case actionTypes.USER_SUCCESS:
+      return {
+        loading: true,
+        users: action.payload,
+      };
+    case actionTypes.USER_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
